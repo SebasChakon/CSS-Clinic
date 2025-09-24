371070acea48d3eb76d1f37f52ae675b22444d20
Constanza-Vilches <cvilchesv@estudiante.uc.cl>
2025-09-24 19:49:56 -0300

    a ver si funciona

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "render_production"
+  # config.active_job.queue_name_prefix = "ayuda_production"
 
   config.action_mailer.perform_caching = false
 
