587a3f590ffb4caa512c02ecb63f0410ff3b8706
Constanza-Vilches <cvilchesv@estudiante.uc.cl>
2025-11-14 00:54:57 -0300

    render no carga ubi

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import GeolocationController from "./geolocation_controller"
-application.register("geolocation", GeolocationController)
